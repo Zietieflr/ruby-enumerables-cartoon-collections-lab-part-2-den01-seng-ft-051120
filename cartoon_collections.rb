b6a384c0f6d225e4c681873b6218e04dd3996557
Logan McGuire <lrmcguire93@gmail.com>
2020-04-24 16:28:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,6 +27,6 @@ def find_valid_calls(planeteer_calls)
   #}}
   #planeteer_calls.map { |call| valid_calls.length.times { |vc_index| (call.to_s === valid_calls[vc_index]) ? (return call) : nil }};
   #nil;
-  
+  planeteer_calls.map { |call| valid_calls.map { |vc| if call === vc 
 end
 
